62f17a6cd5460b78d1c0e1aa0cdf9ca5dcba5519
Sergio Radigales <sergioradigales@gmail.com>
2022-02-25 22:42:18 +0100

    event.preventDefault()

--- js/popUp.js
+++ js/popUp.js
@@ -2,7 +2,8 @@ const func = document.querySelector('.popUpButton')
 const popUp = document.querySelector('.popup')
 const crox = document.querySelector('.fa-times')
 
-func.addEventListener('click', () => {
+func.addEventListener('click', (event) => {
+    event.preventDefault()
     popUp.classList.add('show')
 })
 
